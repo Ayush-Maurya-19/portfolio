c1f8551faeef5316644589b54e32b02db627f95c
ayush-maurya-19 <ayushmaurya1901@gmail.com>
2024-05-16 12:18:45 +0530

    typeAnimation added

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,9 +1,50 @@
+"use client";
+import Image from "next/image";
 import React from "react";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
     <section>
-      <h1 className="text-white">Hello, I'm Ayush Maurya</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl font-extrabold ">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello, I'm{" "}
+            </span>
+            <br />
+            <TypeAnimation
+              sequence={["Ayush Maurya", 1000, "Web Devevloper", 100]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-white text-base sm:text-lg mb-6 lg:text-xl">
+            I'm a full-stack developer
+          </p>
+          <div>
+            <button className="px-6 py-3 rounded-full w-full sm:w-fit bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-orange-400 text-white mr-4">
+              Hire Me
+            </button>
+            <button className="px-1 py-1 inline-block w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 text-white">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="">
+            <Image
+              src="/images/Ayush.png"
+              width={300}
+              height={300}
+              alt="hero"
+            />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
