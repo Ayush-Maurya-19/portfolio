f58262c0a61820d69621138e6b6d8e959e8dd8d7
ayush-maurya-19 <ayushmaurya1901@gmail.com>
2024-05-24 01:24:57 +0530

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
-import ProjectSection from "./components/ProjectSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import Portfolio from "./components/Portfolio";
@@ -14,7 +13,6 @@ export default function Home() {
         <HeroSection />
         <AboutSection />
         <Portfolio />
-        {/* <ProjectSection /> */}
         <EmailSection />
         <Footer />
       </div>
