7bc9a853bc0ac0e0b548def37879f2f5dbcc3ec0
ayush-maurya-19 <ayushmaurya1901@gmail.com>
2024-05-24 01:07:07 +0530

    optimized code

--- src/app/components/Portfolio.jsx
+++ src/app/components/Portfolio.jsx
@@ -9,7 +9,8 @@ const projectsData = [
     title: "Homify (Living Rental Spaces Near You)",
     description: "This is Full Stack Project created using MERN Stack.",
     image: "/images/HomePage1.png",
-    feature1: "In this project I have used React, Node.js, Express.js, MongoDB, and Bootstrap.",
+    feature1:
+      "In this project I have used React, Node.js, Express.js, MongoDB, and Bootstrap.",
     feature2: "Feature 2",
     feature3: "Feature 3",
     gitUrl: "https://github.com/Ayush-Maurya-19/Homify",
@@ -18,7 +19,8 @@ const projectsData = [
   {
     id: 2,
     title: "ThinkInk (A Doodle Game)",
-    description:"This is Full Stack Project created using MERN Stack and Socket.IO",
+    description:
+      "This is Full Stack Project created using MERN Stack and Socket.IO",
     feature1: "Feature 1",
     feature2: "Feature 2",
     feature3: "Feature 3",
@@ -54,16 +56,6 @@ const Portfolio = () => {
   useEffect(() => {
     const sr = ScrollReveal();
 
-    //This is for card but using it.
-    // sr.reveal(".card", {
-    //   delay: 200,
-    //   origin: "bottom",
-    //   distance: "100px",
-    //   duration: 1000,
-    //   interval: 100,
-    //   scale: 0.7,
-    // });
-
     sr.reveal(".project-section:nth-child(odd)", {
       delay: 200,
       origin: "left",
@@ -83,7 +75,6 @@ const Portfolio = () => {
 
   return (
     <>
-      {/* TODO: I want to make this section dynamic. I will create a new component for each project and then I will map through the projects array to display the projects. */}
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
@@ -135,7 +126,7 @@ const Portfolio = () => {
                     </h3>
                     <p>{project.description}</p>
                     <ul className="list-disc ml-5">
-                    <li>{project.feature1}</li>
+                      <li>{project.feature1}</li>
                       <li>{project.feature2}</li>
                       <li>{project.feature3}</li>
                     </ul>
@@ -161,146 +152,6 @@ const Portfolio = () => {
           </section>
         )
       )}
-
-      {/* <section className="text-white project-section">
-        <div className="container mx-auto px-4">
-          <div className="grid grid-flow-row-dense grid-cols-1 md:grid-cols-2">
-            <div className="col-span-1 md:pl-36">
-              <img
-                src="/images/HomePage1.png"
-                alt="project"
-                width="500"
-                height="500"
-                className="rounded-md"
-              />
-            </div>
-            <div className="col-span-1 mt-4 md:mt-0">
-              <div className="card">
-                <h3 className="text-2xl font-bold text-white mb-3">
-                  Homify (Living Rental Spaces Near You)
-                </h3>
-                <p>
-                  This is my first project created using HTML, CSS, and
-                  Javascript.
-                </p>
-                <ul className="list-disc ml-5">
-                  <li>Feature 1</li>
-                  <li>Feature 2</li>
-                  <li>Feature 3</li>
-                </ul>
-                <button className="btn btn-primary rounded-pill mt-5">
-                  View Project
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="text-white mt-10 project-section">
-        <div className="container mx-auto px-4">
-          <div className="grid grid-flow-row-dense gap-4 grid-cols-1 md:grid-cols-2">
-            <div className="col-span-1 order-2 md:order-1 md:ml-36">
-              <div className="card">
-                <h3 className="text-2xl font-bold text-white mb-2">
-                  ThinkInk (A Doodle Game)
-                </h3>
-                <p>
-                  This is my first project created using HTML, CSS, and
-                  Javascript.
-                </p>
-                <ul className="list-disc ml-5">
-                  <li>Feature 1</li>
-                  <li>Feature 2</li>
-                  <li>Feature 3</li>
-                </ul>
-                <button className="btn btn-primary rounded-pill mt-5">
-                  View Project
-                </button>
-              </div>
-            </div>
-            <div className="col-span-1 order-1 md:order-2 mt-4 md:mt-0">
-              <img
-                src="/images/HomePage1.png"
-                alt="project"
-                width="500"
-                height="500"
-                className="rounded-md"
-              />
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="text-white mt-10 project-section">
-        <div className="container mx-auto px-4">
-          <div className="grid grid-flow-row-dense grid-cols-1 md:grid-cols-2">
-            <div className="col-span-1 md:pl-36">
-              <img
-                src="/images/HomePage1.png"
-                alt="project"
-                width="500"
-                height="500"
-                className="rounded-md"
-              />
-            </div>
-            <div className="col-span-1 mt-4 md:mt-0">
-              <div className="card">
-                <h3 className="text-2xl font-bold text-white mb-3">
-                  Homify (Living Rental Spaces Near You)
-                </h3>
-                <p>
-                  This is my first project created using HTML, CSS, and
-                  Javascript.
-                </p>
-                <ul className="list-disc ml-5">
-                  <li>Feature 1</li>
-                  <li>Feature 2</li>
-                  <li>Feature 3</li>
-                </ul>
-                <button className="btn btn-primary rounded-pill mt-5">
-                  View Project
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="text-white mt-10 project-section">
-        <div className="container mx-auto px-4">
-          <div className="grid grid-flow-row-dense gap-4 grid-cols-1 md:grid-cols-2">
-            <div className="col-span-1 order-2 md:order-1 md:ml-36">
-              <div className="card">
-                <h3 className="text-2xl font-bold text-white mb-2">
-                  ThinkInk (A Doodle Game)
-                </h3>
-                <p>
-                  This is my first project created using HTML, CSS, and
-                  Javascript.
-                </p>
-                <ul className="list-disc ml-5">
-                  <li>Feature 1</li>
-                  <li>Feature 2</li>
-                  <li>Feature 3</li>
-                </ul>
-                <button className="btn btn-primary rounded-pill mt-5">
-                  View Project
-                </button>
-              </div>
-            </div>
-            <div className="col-span-1 order-1 md:order-2 mt-4 md:mt-0">
-              <img
-                src="/images/HomePage1.png"
-                alt="project"
-                width="500"
-                height="500"
-                className="rounded-md"
-              />
-            </div>
-          </div>
-        </div>
-      </section> */}
     </>
   );
 };
