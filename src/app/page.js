b1e96914547c86d07174d3dedc95c114ce10c870
ayush-maurya-19 <ayushmaurya1901@gmail.com>
2024-05-25 21:55:50 +0530

    edit

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,9 @@
-import HeroSection from "./components/HeroSection";
-import AboutSection from "./components/AboutSection";
-import EmailSection from "./components/EmailSection";
-import Portfolio from "./components/Portfolio";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] ">
-      <div className="container mt-24 mx-auto px-12 py-4">
-        <HeroSection />
-        <AboutSection />
-        <Portfolio />
-        <EmailSection />
-      </div>
-    </main>
+    <>
+
+    </>
   );
 }
