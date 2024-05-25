3eedfa190d95e4436fc8abe8562a30d43f48c900
ayush-maurya-19 <ayushmaurya1901@gmail.com>
2024-05-25 22:32:56 +0530

    UI changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
-import HeroSection from "./components/HeroSection"
+import AboutSection from "./components/AboutSection";
+import HeroSection from "./components/HeroSection";
 
 const Page = () => {
   return (
     <>
-    <HeroSection />
+      <main className="flex min-h-screen flex-col bg-[#121212] ">
+        <div className="container mt-24 mx-auto px-12 py-4">
+          <HeroSection />
+          <AboutSection />
+        </div>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
