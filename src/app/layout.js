f5403b3329fa6b826131e7c834d8921a6b9e16ab
ayush-maurya-19 <ayushmaurya1901@gmail.com>
2024-05-28 22:43:30 +0530

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
+        <Toaster position="bottom-center" />
         {children}
       </body>
     </html>
